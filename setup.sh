8b6506879321ae04bbff4773412fd61115830e78
Alexander Buntakov <abuntakov@gmail.com>
2016-04-13 01:58:10 +0300

    zsh env

--- setup.sh
+++ setup.sh
@@ -9,3 +9,5 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+chsh -s /bin/zsh
